00ec1c3b576915679690dbe0b60db116c850d93e
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2023-04-24 19:12:22 -0300

    Add product page swiper

--- src/app/main.js
+++ src/app/main.js
@@ -1,37 +1,3 @@
-// Swiper.JS LOGIC
-
-const swiper = new Swiper(".swiper", {
-  direction: "horizontal",
-  loop: true,
-  speed: 1500,
-  effect: "fade",
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    type: "bullets",
-    bulletClass: "swiper-pagination-bullet",
-    bullletActiveClass: "swiper-pagination-bullet-active",
-  },
-
-  // Navigation arrows
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
-});
-
-const swiperEl = document.querySelector(".swiper");
-const buttonEl = document.querySelector(".swiper");
-
-buttonEl.addEventListener("click", () => {
-  swiperEl.swiper.slideNext();
-});
-
 // SHOP Dropdown Menu
 
 const shopButton = document.querySelector(".navbar__shop-menu");
