76f5de7333e9212a831cebca302429644537f849
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2023-05-26 13:05:24 -0300

    feat(products_list): add basic vanilla-lazyload

--- public/app/main.js
+++ public/app/main.js
@@ -1,5 +1,4 @@
 // SHOP Dropdown Menu
-
 const shopButton = document.querySelector(".navbar__shop-menu");
 const shopSubMenu = document.querySelector("#shop-submenu");
 
@@ -28,6 +27,14 @@ document.addEventListener("click", (event) => {
     cartMenu.classList.remove("cart-dropdown--visible");
   }
 });
+
+// VANILLA-LAZYLOAD
+var lazyLoadInstance = new LazyLoad({
+  // Your custom settings go here
+});
+
+// ABOUT US LINK SCROLLDOWN
+
 
 // SORT BY Dropdown Menu
 
